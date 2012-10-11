8d0d710a15e345a01c5d0e83f8281f465165cb2a
Bas Mostert <bas@iweb.co.uk>
2012-10-11 09:17:03 +0100

    Remove jQuery version constraint

--- skin/frontend/base/default/js/balloz/layout-viewer.js
+++ skin/frontend/base/default/js/balloz/layout-viewer.js
@@ -1,14 +1,16 @@
 (function() {
 	function getJQuery(callback) {
-		if (typeof jQuery === "undefined" || jQuery.fn.jquery !== '1.8.2') {
+		if (typeof jQuery === "undefined") {
 			var scriptTag = document.createElement('script');
 			scriptTag.setAttribute("type", "text/javascript");
 			scriptTag.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js");
 			scriptTag.onload = function() {
+				jQuery.noConflict();
 				callback(jQuery);
 			};
 			scriptTag.onreadystatechange = function() {
 				if (this.readyState == 'complete' || this.readyState == 'loaded') {
+					jQuery.noConflict();
 					callback(jQuery);
 				}
 			};
